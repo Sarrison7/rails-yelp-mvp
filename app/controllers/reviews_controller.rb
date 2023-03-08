73bc8a2eb2e9e891352a6b9658ef47e4fe773892
Simon Harrison <simonsy7@aol.com>
2023-03-08 21:15:23 +0000

    yelp

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurants, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entitty
+    end
+  end
+
+  def destroy
+    @review = Review.new.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_paty(@review.restaurant), status: :see_other
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).premit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,16 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 end
+
+
+get '/restaurants', to: 'restaurants#index'
+
+get '/restaurants/new', to: 'restaurants#new'
+
+post '/restaurants', to: 'restaurants#create'
+
+get '/restaurants/:id', to: 'restaurants#show'
+
+get 'restaurants/:restaurants_id/reviews/new', to: 'reviews#new'
+
+post '/restaurants/:restaurant_id/reviews', to: 'reviews#create'
